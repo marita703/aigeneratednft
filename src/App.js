048cd4f85efa774d4f7b8e0ccdbff6d091ec7549
Maria del Mar <chocoinformatics2016@gmail.com>
2023-07-11 15:22:51 +0200

    created front end

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import { useState, useEffect } from 'react';
-import { NFTStorage, File } from 'nft.storage'
-import { Buffer } from 'buffer';
-import { ethers } from 'ethers';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { NFTStorage, File } from "nft.storage";
+import { Buffer } from "buffer";
+import { ethers } from "ethers";
+import axios from "axios";
 
 // Components
-import Spinner from 'react-bootstrap/Spinner';
-import Navigation from './components/Navigation';
+import Spinner from "react-bootstrap/Spinner";
+import Navigation from "./components/Navigation";
 
 // ABIs
-import NFT from './abis/NFT.json'
+import NFT from "./abis/NFT.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-  }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+  };
 
   useEffect(() => {
-    loadBlockchainData()
-  }, [])
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <p>Edit App.js to get started.</p>
-
+      <div className="form">
+        <form>
+          <input type="text" placeholder="Create a name..."></input>
+          <input type="text" placeholder="Create a description..."></input>
+          <input type="submit" value="Create and Mint"></input>
+        </form>
+        <div className="image">
+          <img src="" alt="Ai generated images" />
+        </div>
+      </div>
+      <p>
+        {" "}
+        View&nbsp;
+        <a href="" target="_blank" rel="noreferrer">
+          Metadata
+        </a>
+      </p>
     </div>
   );
 }
