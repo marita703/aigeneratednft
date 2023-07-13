878067e9903c641593d42d9a93f5f474f3820021
Maria del Mar <chocoinformatics2016@gmail.com>
2023-07-13 14:30:40 +0200

    deleted the word FILE from import nft storage, and it worked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { NFTStorage, File } from "nft.storage";
+import { NFTStorage } from "nft.storage";
+//remember not to import file from NFT storage.
 import { Buffer } from "buffer";
 import { ethers } from "ethers";
 import axios from "axios";
